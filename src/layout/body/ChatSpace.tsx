a07a3ba2db1be04c9bc9a79d9a521112d195ff2d
Adgeh <belaybirhanu407@gmail.com>
2024-01-11 11:29:43 +0300

    chat space decorated

--- src/layout/body/ChatSpace.tsx
+++ src/layout/body/ChatSpace.tsx
@@ -32,6 +32,34 @@ export const ChatSpace = () => {
           <Typography color="darkgray">{new Date().toUTCString()}</Typography>
         </Box>
       </Box>
+      <Box sx={{ display: "flex", justifyContent: "right" }}>
+        <Box
+          sx={{
+            p: 1,
+            mt: 1,
+            borderRadius: 5,
+            borderEndEndRadius: 0,
+          }}
+        >
+          <img src="https://static.wikia.nocookie.net/miamivice/images/3/3f/Publicimageltd.jpg"/>
+          <Typography color="darkgray">{new Date().toUTCString()}</Typography>
+        </Box>
+      </Box>
+      <Box display="flex" gap={2}>
+        <Avatar sx={{ mt: 3 }} />
+        <Box
+          sx={{
+            backgroundColor: "lightblue",
+            p: 1,
+            borderRadius: 5,
+            borderEndStartRadius: 0,
+            mt: 1,
+          }}
+        >
+          <Typography variant="body1">Oh! What a memorable image...</Typography>
+          <Typography color="darkgray">{new Date().toUTCString()}</Typography>
+        </Box>
+      </Box>
     </Box>
   );
 };
