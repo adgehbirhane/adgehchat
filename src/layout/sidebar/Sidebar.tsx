f8b587dc092053ade385169dde5abc285f1734f7
Adgeh <belaybirhanu407@gmail.com>
2024-01-11 08:59:43 +0300

    main side bar added

--- src/layout/sidebar/Sidebar.tsx
+++ src/layout/sidebar/Sidebar.tsx
@@ -1,13 +1,38 @@
-import { Box } from "@mui/material";
+import {
+  BreakfastDiningOutlined,
+  GroupTwoTone,
+  HomeTwoTone,
+  MenuTwoTone,
+  PersonTwoTone,
+} from "@mui/icons-material";
+import { Box, Divider, List, ListItemButton, ListItemIcon } from "@mui/material";
 
 export const Sidebar = () => {
   return (
-    <Box
-      bgcolor="skyblue"
-      flex={0.2}
-      p={2}
-    >
-      Side
+    <Box bgcolor="darkgrey" flex={0.1}>
+        <ListItemButton>
+          <ListItemIcon>
+            <MenuTwoTone/>
+          </ListItemIcon>
+        </ListItemButton>
+        <Divider/>
+      <List>
+        <ListItemButton>
+          <ListItemIcon>
+            <HomeTwoTone />
+          </ListItemIcon>
+        </ListItemButton>
+        <ListItemButton>
+          <ListItemIcon>
+            <GroupTwoTone />
+          </ListItemIcon>
+        </ListItemButton>
+        <ListItemButton>
+          <ListItemIcon>
+            <PersonTwoTone />
+          </ListItemIcon>
+        </ListItemButton>
+      </List>
     </Box>
   );
 };
